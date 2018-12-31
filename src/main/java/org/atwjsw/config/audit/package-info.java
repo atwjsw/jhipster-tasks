0b15c619e2d0fa4cfdb7f5796401a08477440beb
Daniel <wendaniel@hotmail.com>
2018-12-31 15:39:29 -0500

    Initial application generated by JHipster-5.7.0

---
+++ src/main/java/org/atwjsw/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.atwjsw.config.audit;
---
+++ src/main/java/org/atwjsw/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.atwjsw.repository;
+
+import org.atwjsw.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
