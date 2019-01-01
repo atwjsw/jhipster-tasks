7118a532e8a95f3cac1c919a2e09475648de03a7
Daniel <wendaniel@hotmail.com>
2019-01-01 01:29:26 -0500

    generate jhipster app and task entity

--- src/main/java/org/atwjsw/config/CacheConfiguration.java
+++ src/main/java/org/atwjsw/config/CacheConfiguration.java
@@ -38,6 +38,7 @@ public class CacheConfiguration {
             cm.createCache(org.atwjsw.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(org.atwjsw.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(org.atwjsw.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(org.atwjsw.domain.Task.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
